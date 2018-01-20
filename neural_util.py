softmax fixes

neural_util.py
@@ -137,14 +137,14 @@ def error_rate2(res, givenLabel):
             err += 1
     return ((float)(err)) / givenLabel.size
 
-def error_rate2(res, givenLabel):
+def error_rate3(res, givenLabel):
     err = 0
     labels1 = np.argmax(res,axis=1)
     labels2 = np.argmax(givenLabel,axis=1)
-    for x in range(0, len(res)):
+    for x in range(0, len(labels2)):
         if labels1[x] != labels2[x]:
             err += 1
-    return ((float)(err)) / givenLabel.size
+    return ((float)(err)) / len(givenLabel)
 
 # Custum round function to round by clipping.
 def round(res):
@@ -154,7 +154,7 @@ def round(res):
 def oneHotEncoding(labels):
     res = np.zeros((labels.shape[0],10))
     for i in range(0,len(labels)):
-        res[i,labels[i]] = 1
+        res[i,int(labels[i])] = 1
     return res
 
 # Custom function to find derivative of absolute value function

soft_gradient_descent.py
@@ -31,20 +31,11 @@ def gradient_descent(dataM, labels, classes, neural, numIter, n0, T, test_images
 
     # Loop through data
     for t in range(0, numIter):
-        print t
-        numMini=1
-        # Caluclate the size of the minibatch.
-        size = train_images.shape[0]/numMini; 
-        # Grab the error rate before training.
-        errorOld = ut.error_rate2(neural.run(holdout_images), holdout_labels)
-
-        # Step size.
+        errorOld = ut.error_rate3(neural.run(holdout_images), holdout_labels)
         n = n0/(1+t/float(T))
-
-        W = W + n * gradient2(neural.run(train_images),train_labels, train_images)
-
-        # Calculate the error rate after training.
-        errorNew = ut.error_rate2(neural.run(holdout_images), holdout_labels)
+        W = W + n * gradient2(neural.run(train_images),train_labels, train_images) + n * regConst * lx_gradient(W, train_images.shape[0], normNum)
+        neural.W = W
+        errorNew = ut.error_rate3(neural.run(holdout_images), holdout_labels)
         print errorNew
 
         # Logic to detect if we should stop due to hold-out.
@@ -111,7 +102,8 @@ def gradient(Y, X, T, c):
     return res
 
 def gradient2(Y,T,X):
-    return np.matmul(X.T,np.subtract(T,Y))
+    res = np.matmul(np.transpose(X),np.subtract(T,Y))
+    return res
 
 #def updateStep(self, t, T):
 #    self.n = self.n / (1 + t/T)

softmax_regression.py
@@ -23,7 +23,7 @@ class SoftMax:
         
         def run(self, dataM):
             res = np.matmul(dataM, self.W);
-            res = np.exp(res);
+            res = np.exp(np.clip(res,-100,100))
             res_sum = res.sum(axis=1)
             res = res / res_sum[:, np.newaxis]
             return res